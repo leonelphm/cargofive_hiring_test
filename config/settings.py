140b33a28dcf80688e7f9595471023c4ee43c2fc
Leonel Paolo Hernandez Macchiarulo <leonelphm@gmail.com>
2022-02-03 08:41:01 -0400

    configurado static para heroku

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 LOGIN_URL = "/"
