1e79edf6a2485664346f7d758a632374b086e722
Leonel Paolo Hernandez Macchiarulo <leonelphm@gmail.com>
2022-02-03 08:27:01 -0400

    agregado directorios estaticos

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -145,8 +145,14 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    SOURCES_URL
 ]
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 LOGIN_URL = "/"
 
